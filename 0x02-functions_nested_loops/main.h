47ea521ead727efd77b82ad019e4c4fe2b2f07a6
root <root@kali>
2021-08-31 20:39:07 +0300

    update

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+0-holberton.c      104-fibonacci.c         3-islower.c           8-24_hours.c
+100-times_table.c  10-add.c                4-isalpha.c           9-times_table.c
+101-natural.c      11-print_to_98.c        5-sign.c              
+102-fibonacci.c    1-alphabet.c            6-abs.c
+103-fibonacci.c    2-print_alphabet_x10.c  7-print_last_digit.c
