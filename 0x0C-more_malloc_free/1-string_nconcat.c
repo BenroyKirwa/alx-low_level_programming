59cc2ed56b632c23065129c1ed8cb8ca44de0c17
root <root@kali>
2021-09-24 11:33:05 +0300

    updating 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,63 +1,56 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string whose length is to be returned
- * Return: length of string
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
  */
-int _strlen(char *s)
-{
-	int i = 0;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
 	return (i);
 }
+
 /**
- * *string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: size of memory allocated for s2 in bytes
- * Return: pointer shall point to a newly allocated space in
- * memory, which contains s1, followed by the first n bytes of
- * s2, and null terminated or NULL
+ * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l2 = _strlen(s2);
-	unsigned int l1 = _strlen(s1);
-	unsigned int i, j;
-	char *p;
+	char *ptr;
+	int num, len, i, j;
 
-	if (n >= l2)
-	{
-		n = l2;
-	}
-	if (s1 == NULL)
-	{
-		s1 = "Hello";
-	}
+	num = n;
+
+	if (s1 == NULL) /* account for NULL strings */
+		s1 = "";
 	if (s2 == NULL)
-	{
-		s2 = "Hello";
-	}
-	p = malloc(sizeof(*p) * (l1 + n + 1));
-	if (p == NULL)
-	{
+		s2 = "";
+	if (num < 0) /* account for negative n bytes */
 		return (NULL);
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		p[i] = s1[i];
-	}
-	for (j = 0; j < n && s2[j] != '\0'; j++)
-	{
-		p[i] = s2[j];
-		i++;
-	}
-	p[i] = '\0';
-	return (p);
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
+
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+
+	return (ptr);
 }
